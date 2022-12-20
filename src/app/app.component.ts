f05cfe52a481e64761079ae75221080143b2ef41
PAVasconcelos <59854094+PAVasconcelos@users.noreply.github.com>
2022-12-20 23:38:51 +0000

    MVP
    MVP final features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { bottom } from '@popperjs/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,52 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 
 export class AppComponent {
-  title = 'pvportfolio';
+
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  public isNavBarOpened: boolean = false;
+
+  ngOnInit() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
+
+  public setIsNavBarOpened(value: boolean): void {
+    this.isNavBarOpened = value;
+  }
+
+  public scrollToSection(section: string): void {
+    if (section === "About" && this.isNavBarOpened == false && this.getScreenWidth > 575) {
+      scrollTo(0, 70);
+    }
+    else if (section === "About" && this.isNavBarOpened == true) {
+      scrollTo(0, 200);
+    }
+    else if (section === "About" && this.isNavBarOpened == true && this.getScreenWidth < 575) {
+      scrollTo(0, 200);
+    }
+    else if (section === "Career" && this.isNavBarOpened == false && this.getScreenWidth > 575) {
+      scrollTo(0, 410);
+    }
+    else if (section === "Career" && this.isNavBarOpened == true && this.getScreenWidth > 575) {
+      scrollTo(0, 585);
+    }
+    else if (section === "Career" && this.isNavBarOpened == true && this.getScreenWidth < 575 && this.getScreenWidth > 391) {
+      scrollTo(0, 710);
+    }
+    else if (section === "Career" && this.isNavBarOpened == true && this.getScreenWidth < 390) {
+      scrollTo(0, 780);
+    }
+    else if (section === "Skills" || section === "FindMe") {
+      scrollTo(0, 99999);
+    }
+
+  }
+
 }
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  // @Output to send data to parent component.
+  @Output() navBarSelectedItemEvent = new EventEmitter<string>();
+  @Output() isNavBarOpenedEvent = new EventEmitter<boolean>();
+  public isNavBarOpened: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  } 
+
+  public toParentSelectedNavBar(value: string): void {
+    this.navBarSelectedItemEvent.emit(value);
+  }
+
+  public toParentNavBarOpened(): void {
+    this.isNavBarOpenedEvent.emit(this.isNavBarOpened);
+  }
+
+  public navtoggle(): void {
+    this.isNavBarOpened = !this.isNavBarOpened;
+    this.toParentNavBarOpened();
   }
 
 }
